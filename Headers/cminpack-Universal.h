1e3948882eaa0695769eadadde935226fcb16ae0
Karolis Stasaitis <stkarolis@gmail.com>
2015-11-07 00:46:50 +0100

    First Version Release

---
+++ Headers/cminpack-Universal.h
@@ -0,0 +1,20 @@
+//
+//  cminpack-Universal.h
+//  cminpack
+//
+//  Created by Karolis Stasaitis on 07/11/15.
+//  Copyright © 2015 delanoir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cminpack.
+FOUNDATION_EXPORT double cminpackVersionNumber;
+
+//! Project version string for cminpack.
+FOUNDATION_EXPORT const unsigned char cminpackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cminpack_tvOS/PublicHeader.h>
+#import <cminpack/minpack.h>
+
+
